b36b4227375607e85bf5668c360805912fbd54f7
t-natori <102852756+t-natori@users.noreply.github.com>
2022-07-05 11:58:55 +0000

    [add]migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_102728) do
+ActiveRecord::Schema.define(version: 2022_07_05_115555) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -85,9 +85,9 @@ ActiveRecord::Schema.define(version: 2022_06_17_102728) do
     t.integer "parking"
     t.text "detail"
     t.boolean "status", default: true, null: false
-    t.decimal "score", precision: 5, scale: 3
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "score", precision: 5, scale: 3
   end
 
   create_table "tags", force: :cascade do |t|
